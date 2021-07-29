c32e79aa8e5682837d32f20b9bcf7f9e8a60fcc4
Michael PS <michael.plasomscott@hotmail.co.uk>
2021-07-29 15:02:47 +0100

    penalty fare no entry/exit

--- lib/journey.rb
+++ lib/journey.rb
@@ -3,22 +3,27 @@ class Journey
 
   def initialize(entry_station = nil) 
     @entry_station = entry_station
+    @exit_station = nil
   end
 
   def complete?
-
+    @entry_station && @exit_station 
   end
 
   def fare
-    PENALTY_FARE
+    if complete?
+      return 1
+    else PENALTY_FARE
+    end
   end
 
   def finish(station)
+    @exit_station = station
     self
   end
 
   def entry_station
-    @entry_station
+    @entry_station[:entry_station]
   end
 
 end
